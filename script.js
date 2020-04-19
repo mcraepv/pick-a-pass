07d44a8055d6721527b932b654e2462b0eab98dc
mcraepv <61661042+mcraepv@users.noreply.github.com>
2020-04-19 18:01:54 -0500

    significantly compresses code

--- script.js
+++ script.js
@@ -13,138 +13,54 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//begins by checking for character types
 function generatePassword() {
   var lower = confirm("Should your password contain lowercase letters?");
   var upper = confirm("Should your password contain uppercase letters?");
   var numbers = confirm("Should your password contain numbers?");
   var special = confirm("Should your password contain special characters?");
+  //defines character types
+  var characters = '';
+  var charNumbers = "1234567890";
+  var charLower = "abcdefghijklmnopqrstuvwxyz";
+  var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var charSpecial = " /'\"!#$%&)(*+,-.:;<>=?@[]\\^_~`{|}"
+
+  //checks that at least 1 character type is true
+  while (lower === false && upper === false && numbers === false && special === false) {
+    alert("Please choose at least one character type")
+    var lower = confirm("Should your password contain lowercase letters?");
+    var upper = confirm("Should your password contain uppercase letters?");
+    var numbers = confirm("Should your password contain numbers?");
+    var special = confirm("Should your password contain special characters?");
+  }
+
   var length = prompt("How long should your password be (Please choose a character length between 8 and 128.)");
 
+  //checks that password length is within limits
   while (length < 8 || length > 128) {
     alert("Please choose a character length between 8 and 128.");
     var length = prompt("How long should your password be (Please choose a character length between 8 and 128.)");
   }
-
-  if (lower === true && upper === true && numbers === true && special === true) {
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 /'\"!#$%&)(*+,-.:;<>=?@[]\\^_~`{|}";
-    var charLength = characters.length;
-    var output = '';
-    for (var i = 0; i < length; i++) {
-      output += characters.charAt(Math.floor(Math.random() * charLength));
-    }
-    return output;
-  } else if (lower === true && upper === true && numbers === false && special === true) {
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz /'\"!#$%&)(*+,-.:;<>=?@[]\\^_~`{|}";
-    var charLength = characters.length;
-    var output = '';
-    for (var i = 0; i < length; i++) {
-      output += characters.charAt(Math.floor(Math.random() * charLength));
-    }
-    return output;
-  } else if (lower === true && upper === true && numbers === true && special === false) {
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-    var charLength = characters.length;
-    var output = '';
-    for (var i = 0; i < length; i++) {
-      output += characters.charAt(Math.floor(Math.random() * charLength));
-    }
-    return output;
-  } else if (lower === false && upper === true && numbers === true && special === true) {
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 /'\"!#$%&)(*+,-.:;<>=?@[]\\^_~`{|}";
-    var charLength = characters.length;
-    var output = '';
-    for (var i = 0; i < length; i++) {
-      output += characters.charAt(Math.floor(Math.random() * charLength));
-    }
-    return output;
-  } else if (lower === true && upper === false && numbers === true && special === true) {
-    var characters = "abcdefghijklmnopqrstuvwxyz1234567890 /'\"!#$%&)(*+,-.:;<>=?@[]\\^_~`{|}";
-    var charLength = characters.length;
-    var output = '';
-    for (var i = 0; i < length; i++) {
-      output += characters.charAt(Math.floor(Math.random() * charLength));
-    }
-    return output;
-  } else if (lower === true && upper === false && numbers === true && special === false) {
-    var characters = "abcdefghijklmnopqrstuvwxyz1234567890";
-    var charLength = characters.length;
-    var output = '';
-    for (var i = 0; i < length; i++) {
-      output += characters.charAt(Math.floor(Math.random() * charLength));
-    }
-    return output;
-  } else if (lower === true && upper === true && numbers === false && special === false) {
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    var charLength = characters.length;
-    var output = '';
-    for (var i = 0; i < length; i++) {
-      output += characters.charAt(Math.floor(Math.random() * charLength));
-    }
-    return output;
-  } else if (lower === true && upper === false && numbers === false && special === true) {
-    var characters = "abcdefghijklmnopqrstuvwxyz /'\"!#$%&)(*+,-.:;<>=?@[]\\^_~`{|}";
-    var charLength = characters.length;
-    var output = '';
-    for (var i = 0; i < length; i++) {
-      output += characters.charAt(Math.floor(Math.random() * charLength));
-    }
-    return output;
-  } else if (lower === false && upper === true && numbers === true && special === false) {
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-    var charLength = characters.length;
-    var output = '';
-    for (var i = 0; i < length; i++) {
-      output += characters.charAt(Math.floor(Math.random() * charLength));
-    }
-    return output;
-  } else if (lower === false && upper === true && numbers === false && special === true) {
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ /'\"!#$%&)(*+,-.:;<>=?@[]\\^_~`{|}";
-    var charLength = characters.length;
-    var output = '';
-    for (var i = 0; i < length; i++) {
-      output += characters.charAt(Math.floor(Math.random() * charLength));
-    }
-    return output;
-  } else if (lower === false && upper === false && numbers === true && special === true) {
-    var characters = "1234567890 /'\"!#$%&)(*+,-.:;<>=?@[]\\^_~`{|}";
-    var charLength = characters.length;
-    var output = '';
-    for (var i = 0; i < length; i++) {
-      output += characters.charAt(Math.floor(Math.random() * charLength));
-    }
-    return output;
-  } else if (lower === true && upper === false && numbers === false && special === false) {
-    var characters = "abcdefghijklmnopqrstuvwxyz";
-    var charLength = characters.length;
-    var output = '';
-    for (var i = 0; i < length; i++) {
-      output += characters.charAt(Math.floor(Math.random() * charLength));
-    } return output;
-  } else if (lower === false && upper === true && numbers === false && special === false) {
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var charLength = characters.length;
-    var output = '';
-    for (var i = 0; i < length; i++) {
-      output += characters.charAt(Math.floor(Math.random() * charLength));
-    }
-    return output;
-  } else if (lower === false && upper === false && numbers === true && special === false) {
-    var characters = "1234567890";
-    var charLength = characters.length;
-    var output = '';
-    for (var i = 0; i < length; i++) {
-      output += characters.charAt(Math.floor(Math.random() * charLength));
-    }
-    return output;
-  } else if (lower === false && upper === false && numbers === false && special === true) {
-    var characters = " /'\"!#$%&)(*+,-.:;<>=?@[]\\^_~`{|}";
-    var charLength = characters.length;
-    var output = '';
-    for (var i = 0; i < length; i++) {
-      output += characters.charAt(Math.floor(Math.random() * charLength));
-    }
-    return output;
-  } else {
-    alert("Please be sure to choose at least one type of character.");
+  //checks which character types user selected
+  if (lower === true) {
+    characters += charLower;
   }
+  if (upper === true) {
+    characters += charUpper;
+  }
+  if (numbers === true) {
+    characters += charNumbers;
+  }
+  if (special === true) {
+    characters += charSpecial;
+  }
+  var charLength = characters.length;
+  var output = '';
+
+  //generates password randomly from selected character types
+  for (var i = 0; i < length; i++) {
+    output += characters.charAt(Math.floor(Math.random() * charLength));
+  }
+  return output;
 }
